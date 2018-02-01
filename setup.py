664cffbec8453626d8ccbacf0bb7b5a91ffffc11
ejrb <ereadebanham@gmail.com>
2018-02-01 13:42:57 +0000

    setup.py replaces requirement.txt

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='travelnest',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ejrb',
+    author_email='',
+    install_requires=[
+        'pytest',
+        'beautifulsoup4',
+        'requests'
+    ],
+    description=''
+)
